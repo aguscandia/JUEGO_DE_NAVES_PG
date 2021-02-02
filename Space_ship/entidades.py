9fe3a97bad4bb7be4c95994379b6b038413e6db8
agustin <a.ti.21.ac@gmail.com>
2021-02-01 21:25:36 -0300

    Retardo_animacion_asteroide

--- Space_ship/entidades.py
+++ Space_ship/entidades.py
@@ -1,18 +1,32 @@
 import pygame as pg
-from Space_ship import GAME_DIMENSIONS
+from Space_ship import GAME_DIMENSIONS, FPS
 
 import random
 import sys
 
 pg.init()
 
+
+class nave:
+    def __init__(self, x, y, vy):
+        self.x = x
+        self.y = y
+        self.vy = vy
+
+        self.image = pg.image.load("recursos/imagenes/nave-75x50.png")
+
+
+
+
 class asteroide:
-    imagenes_files = ['asteroides_01.png','asteroides_02.png','asteroides_03.png','asteroides_04.png',  'asteroides_05.png',
+    imagenes_files = ['asteroides_01.png', 'asteroides_02.png', 'asteroides_03.png', 'asteroides_04.png',  'asteroides_05.png',
                         'asteroides_06.png',  'asteroides_07.png', 'asteroides_08.png', 'asteroides_09.png', 'asteroides_10.png',
                         'asteroides_11.png',  'asteroides_12.png', 'asteroides_13.png', 'asteroides_14.png', 'asteroides_15.png',
                         'asteroides_16.png',  'asteroides_18.png', 'asteroides_19.png', 'asteroides_20.png', 'asteroides_21.png',
                         'asteroides_22.png',  'asteroides_23.png', 'asteroides_24.png', 'asteroides_25.png', 'asteroides_26.png', 
                         'asteroides_27.png',  'asteroides_28.png', 'asteroides_29.png', 'asteroides_30.png', 'asteroides_31.png']
+
+    retardo_anim = 5  # mientras mas aumenta en valor mas lenta es la animacion
 
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -20,15 +34,15 @@ class asteroide:
         self.vx = vx
         self.vy = vy
         self.image_act = 0     # contador inicializado en 0
+        self.ciclos_tras_refresco = 0  # contador de ciclos para  velocidad de la animacion
         self.imagenes = self.cargaImagenes()
-        self.image = self.imagenes[self.image_act]
+        self.image = self.imagenes[self.image_act] 
 
         self.rect = self.image.get_rect(x=x,y=y)
 
     def cargaImagenes(self):
         lista_imagenes = []
         for img in self.imagenes_files:
-            print(img)
             lista_imagenes.append(pg.image.load(f"recursos/imagenes/{img}"))
 
         return lista_imagenes
@@ -48,12 +62,14 @@ class asteroide:
 
         # Gestionar imagen activa (disfraz de asteroide)
 
-        self.image_act += 1
-        if self.image_act >= len(self.imagenes):
-            self.image_act = 0
+        self.ciclos_tras_refresco += 1
+
+        if self.ciclos_tras_refresco % self.retardo_anim == 0:
+            self.image_act += 1
+            if self.image_act >= len(self.imagenes):
+                self.image_act = 0
+        
         self.image = self.imagenes[self.image_act]
-
-
 
 
     def actualizar(self):
@@ -68,25 +84,30 @@ class Game:
         self.clock = pg.time.Clock()
         self.pantalla = pg.display.set_mode( GAME_DIMENSIONS )
         pg.display.set_caption("Futuro space ship")
-        self.asteroide = asteroide( 928, 236,6, 0)
+        self.asteroide = asteroide( 928, 236, 5, 0)
+        self.nave = nave( 10, 275, 0)
 
-
-    def bucle_principal(self):           
+    # bucle principal   
+    def bucle_principal(self):        
         game_over = False
 
         while not game_over:
+            # Control de eventos
             events = pg.event.get()
-            self.clock.tick(60)
+            self.clock.tick(FPS)
             for event in events:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
         
 
+            # Zona de Actualización de elementos del juego
 
             self.asteroide.actualizar()
+
             self.pantalla.fill((11, 44, 94))
-
             self.pantalla.blit(self.asteroide.image, (self.asteroide.rect.x, self.asteroide.rect.y))
+            self.pantalla.blit(self.nave.image, (self.nave.x, self.nave.y))
 
+            # Zona de refrescar pantalla
             pg.display.flip()
