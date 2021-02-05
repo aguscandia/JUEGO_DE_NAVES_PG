03d054f6c387844e23f1aa199946049da20d7ba2
agustin <a.ti.21.ac@gmail.com>
2021-02-05 11:12:45 -0300

    clase_aster_en_lista

--- Space_ship/entidades.py
+++ Space_ship/entidades.py
@@ -32,10 +32,26 @@ class nave:
         else:
             self.vy = 0
 
+class Asteroidex3():
+    w=50
+    h=50
+    def __init__(self):
+        self.x = -self.w
+        self.y = random.randint(0, 550)
+        self.vx = 4
+        self.image = pg.image.load("recursos/imagenes/aster1-50x50.png")
+
+
+    def actualizar(self):
+        self.x -= self.vx
+        if self.x <= -50:
+            self.x = 850
+            self.y = random.randint(0, 550)
+            self.vx = random.randint(2, 15)
 
 
 
-class asteroide:
+class asteroide():
     imagenes_files = ['asteroides_01.png', 'asteroides_02.png', 'asteroides_03.png', 'asteroides_04.png',  'asteroides_05.png',
                         'asteroides_06.png',  'asteroides_07.png', 'asteroides_08.png', 'asteroides_09.png', 'asteroides_10.png',
                         'asteroides_11.png',  'asteroides_12.png', 'asteroides_13.png', 'asteroides_14.png', 'asteroides_15.png',
@@ -61,10 +77,7 @@ class asteroide:
         lista_imagenes = []
         for img in self.imagenes_files:
             lista_imagenes.append(pg.image.load(f"recursos/imagenes/{img}"))
-
         return lista_imagenes
-
-
 
     def actualizar_posicion(self):
 
@@ -88,13 +101,10 @@ class asteroide:
         
         self.image = self.imagenes[self.image_act]
 
-    
 
     def actualizar(self):
         self.actualizar_posicion()
         self.actualizar_imagen()
-
-
 
 
 class Game:
@@ -103,6 +113,17 @@ class Game:
         self.pantalla = pg.display.set_mode( GAME_DIMENSIONS )
         pg.display.set_caption("Futuro space ship")
         self.asteroide = asteroide( 928, 236, 5, 0)
+        self.asteroide2 = asteroide( 928, 116, 4, 0)
+        self.asteroide3 = asteroide( 928, 126, 3, 0)
+        self.asteroide4 = asteroide( 928, 116, 2, 0)
+
+
+        self.aster = []
+        for i in range(random.randint(2, 7)):
+            ax3 = Asteroidex3()
+            ax3.vx = random.randint(2, 15)
+            self.aster.append(ax3)
+        
         self.nave = nave( 10, 275, 0)
 
     # bucle principal   
@@ -121,15 +142,35 @@ class Game:
             self.nave.manejar_eventos()
 
 
-
-
             # Zona de Actualización de elementos del juego
+
 
             self.nave.actualizar()
             self.asteroide.actualizar()
 
+            self.asteroide2.actualizar()
+            self.asteroide3.actualizar()
+            self.asteroide4.actualizar()
+
+
+
+            for aster in self.aster :
+                aster.actualizar()
+
+            # Zona de pintado de elementos 
+            
             self.pantalla.fill((11, 44, 94))
             self.pantalla.blit(self.asteroide.image, (self.asteroide.rect.x, self.asteroide.rect.y))
+
+            self.pantalla.blit(self.asteroide2.image, (self.asteroide2.rect.x, self.asteroide2.rect.y))
+            self.pantalla.blit(self.asteroide3.image, (self.asteroide3.rect.x, self.asteroide3.rect.y))
+            self.pantalla.blit(self.asteroide4.image, (self.asteroide4.rect.x, self.asteroide4.rect.y))
+
+
+
+            for aster in self.aster :
+                self.pantalla.blit(aster.image, (aster.x, aster.y))
+
             self.pantalla.blit(self.nave.image, (self.nave.x, self.nave.y))
 
             # Zona de refrescar pantalla
