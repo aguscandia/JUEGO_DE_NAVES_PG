e992b078054aaf7023223b04446823b9227cd2ef
agustin <a.ti.21.ac@gmail.com>
2021-02-12 09:57:27 -0300

    Fin_ciclo_asteroides_Aterrizaje_nave

--- Space_ship/entidades.py
+++ Space_ship/entidades.py
@@ -9,10 +9,12 @@ pg.init()
 # fuente
 pg.font.init()
 
-def create_font(t, s=72, c=(255,255,0), b=False, i=False):
+
+def create_font(t, s=72, c=(255, 255, 0), b=False, i=False):
     font = pg.font.SysFont("Arial", s, bold=b, italic=i)
     text = font.render(t, True, c)
     return text
+
 
 GAME_OVER_FONT = create_font("GAME OVER")
 SURF = pg.display.set_mode(GAME_DIMENSIONS)
@@ -27,7 +29,7 @@ class Nivel:
         self.update_nivel()
         self.meta_nivel = m
         self.finalizando = False
-    
+
     def update_nivel(self):
         for i in range(random.randint(2, 7)):
             ax3 = Asteroidex3()
@@ -51,17 +53,26 @@ class Nivel:
     def tieneAsteroides(self):
         totalAsteroides = []
         for bigAster in self.bigAsters:
-                totalAsteroides.append(bigAster.rect.x < 0)
+            totalAsteroides.append(bigAster.rect.x < - pg.Surface.get_width(bigAster.image))
         for aster in self.aster:
-                totalAsteroides.append(aster.x < 0)
+            totalAsteroides.append(aster.x < - pg.Surface.get_width(aster.image))
         return not all(totalAsteroides)
+
+    def restart(self):
+        self.bigAsters = []
+        self.aster = []
+        self.update_nivel()
 
 
 class Explote(pg.sprite.Sprite):
-    imagenes_files = ['explote100x100_01.png', 'explote100x100_02.png', 'explote100x100_03.png', 'explote100x100_04.png',  'explote100x100_05.png',
-                      'explote100x100_06.png',  'explote100x100_07.png', 'explote100x100_08.png', 'explote100x100_09.png', 'explote100x100_10.png',
-                      'explote100x100_11.png',  'explote100x100_12.png', 'explote100x100_13.png', 'explote100x100_14.png', 'explote100x100_15.png',
-                      'explote100x100_16.png',  'explote100x100_18.png', 'explote100x100_19.png', 'explote100x100_20.png']
+    imagenes_files = ['explote100x100_01.png', 'explote100x100_02.png', 'explote100x100_03.png',
+                      'explote100x100_04.png', 'explote100x100_05.png',
+                      'explote100x100_06.png', 'explote100x100_07.png', 'explote100x100_08.png',
+                      'explote100x100_09.png', 'explote100x100_10.png',
+                      'explote100x100_11.png', 'explote100x100_12.png', 'explote100x100_13.png',
+                      'explote100x100_14.png', 'explote100x100_15.png',
+                      'explote100x100_16.png', 'explote100x100_18.png', 'explote100x100_19.png',
+                      'explote100x100_20.png']
 
     def __init__(self):
         super(Explote, self).__init__()
@@ -71,16 +82,14 @@ class Explote(pg.sprite.Sprite):
         self.rect = pg.Rect(5, 5, 150, 198)
         self.ciclos_tras_refresco = 0
         self.retardo_anim = 5
-        
 
     def update(self):
         self.ciclos_tras_refresco += 1
         if self.ciclos_tras_refresco % self.retardo_anim == 0:
             self.image_act += 1
             if self.image_act >= len(self.imagenes):
-                self.image_act = 0       
+                self.image_act = 0
         self.image = self.imagenes[self.image_act]
-
 
     def cargaImagenes(self):
         lista_imagenes = []
@@ -96,11 +105,13 @@ class Explote(pg.sprite.Sprite):
         pg.mixer.init()
         pg.mixer.music.load("recursos/audio/sonido-1.mp3")
         pg.mixer.music.play()
-
+        pg.mixer.music.set_volume(0.1)
 
 class nave:
 
     def __init__(self, x, y, vy):
+        self.rotacion = 0
+        self.vc = 10   # velocidad crucero
         self.x = x
         self.y = y
         self.vy = vy
@@ -119,21 +130,36 @@ class nave:
     def getHeight(self):
         return self.height
 
-
     def setDimension(self):
         self.height = pg.Surface.get_height(self.image)
         self.width = pg.Surface.get_width(self.image)
 
-
     def actualizar(self):
         self.y += self.vy
-        self.rect.top += self.vy 
+        self.rect.top += self.vy
         if self.y + 50 >= GAME_DIMENSIONS[1]:
             self.y = GAME_DIMENSIONS[1] - 50
             self.rect.top = GAME_DIMENSIONS[1] - 50
         if self.y <= 0:
             self.y = 0
             self.rect.top = 0
+
+    def naveAterrizando(self):
+        # si la nave esta por abajo de la mitad de la pantalla tiene que acelerar hacia arriba
+        # si la nave esta por arriba de la mitad de la pantalla tiene que acelerar hacia abajo
+        # si la nave esta esta en el centro no tiene que acelerar ni para arriba ni para abajo tiene que acelerar en x
+        # si la nave esta al final de la pantalla menos su ancho girar 180° y no aterrizar mas.
+        if self.y < GAME_DIMENSIONS[1] / 2:
+            self.y += self.vc
+        if self.y > GAME_DIMENSIONS[1] / 2:
+            self.y -= self.vc
+        if self.y == GAME_DIMENSIONS[1] / 2:
+            self.x += self.vc
+        if self.x == GAME_DIMENSIONS[0] - self.width:
+            self.rotacion -= 2 % 360
+            self.image = pg.transform.rotate(self.image, self.rotacion)
+
+
 
     def manejar_eventos(self):
         # metodo movimiento de nave con get pressed
@@ -146,10 +172,10 @@ class nave:
             self.vy = 0
 
 
-
 class Asteroidex3():
-    w=50
-    h=50
+    w = 50
+    h = 50
+
     def __init__(self):
         self.x = -self.w
         self.y = random.randint(0, 550)
@@ -157,8 +183,6 @@ class Asteroidex3():
         self.image = pg.image.load("recursos/imagenes/aster1-50x50.png")
         # self.rect = self.image.get_rect(x = self.x, y = self.y)
         self.rect = pg.Surface.get_rect(self.image)
-
-
 
     def actualizar(self, finalizando):
         self.x -= self.vx
@@ -171,15 +195,19 @@ class Asteroidex3():
             self.vx = random.randint(2, 10)
 
 
-
-
 class asteroide():
-    imagenes_files = ['asteroides_01.png', 'asteroides_02.png', 'asteroides_03.png', 'asteroides_04.png',  'asteroides_05.png',
-                        'asteroides_06.png',  'asteroides_07.png', 'asteroides_08.png', 'asteroides_09.png', 'asteroides_10.png',
-                        'asteroides_11.png',  'asteroides_12.png', 'asteroides_13.png', 'asteroides_14.png', 'asteroides_15.png',
-                        'asteroides_16.png',  'asteroides_18.png', 'asteroides_19.png', 'asteroides_20.png', 'asteroides_21.png',
-                        'asteroides_22.png',  'asteroides_23.png', 'asteroides_24.png', 'asteroides_25.png', 'asteroides_26.png', 
-                        'asteroides_27.png',  'asteroides_28.png', 'asteroides_29.png', 'asteroides_30.png', 'asteroides_31.png']
+    imagenes_files = ['asteroides_01.png', 'asteroides_02.png', 'asteroides_03.png', 'asteroides_04.png',
+                      'asteroides_05.png',
+                      'asteroides_06.png', 'asteroides_07.png', 'asteroides_08.png', 'asteroides_09.png',
+                      'asteroides_10.png',
+                      'asteroides_11.png', 'asteroides_12.png', 'asteroides_13.png', 'asteroides_14.png',
+                      'asteroides_15.png',
+                      'asteroides_16.png', 'asteroides_18.png', 'asteroides_19.png', 'asteroides_20.png',
+                      'asteroides_21.png',
+                      'asteroides_22.png', 'asteroides_23.png', 'asteroides_24.png', 'asteroides_25.png',
+                      'asteroides_26.png',
+                      'asteroides_27.png', 'asteroides_28.png', 'asteroides_29.png', 'asteroides_30.png',
+                      'asteroides_31.png']
 
     retardo_anim = 5  # mientras mas aumenta en valor mas lenta es la animacion
 
@@ -188,11 +216,11 @@ class asteroide():
         self.y = random.randint(2, 15)
         self.vx = 5
         self.vy = 0
-        self.image_act = 0     # contador inicializado en 0
+        self.image_act = 0  # contador inicializado en 0
         self.ciclos_tras_refresco = 0  # contador de ciclos para  velocidad de la animacion
         self.imagenes = self.cargaImagenes()
-        self.image = self.imagenes[self.image_act] 
-        self.rect = self.image.get_rect(x = self.x, y = self.y)
+        self.image = self.imagenes[self.image_act]
+        self.rect = self.image.get_rect(x=self.x, y=self.y)
         # self.circleRadius = pg.Surface.get_rect(self.image)[0]/2
 
     def cargaImagenes(self):
@@ -221,7 +249,6 @@ class asteroide():
                 self.image_act = 0
         self.image = self.imagenes[self.image_act]
 
-
     def actualizar(self, finalizando):
         self.actualizar_posicion(finalizando)
         self.actualizar_imagen()
@@ -229,8 +256,8 @@ class asteroide():
 
 class Game:
     def __init__(self):
-        self.clock = pg.time.Clock()      
-        self.pantalla = pg.display.set_mode( GAME_DIMENSIONS )
+        self.clock = pg.time.Clock()
+        self.pantalla = pg.display.set_mode(GAME_DIMENSIONS)
         self.bg = pg.image.load("recursos/imagenes/fondo-800x600.jpg")
         pg.display.set_caption("Futuro space ship")
         self.stop_level = False
@@ -242,7 +269,7 @@ class Game:
         self.puntos = 0
         self.goalRect = pg.Rect(0, 0, 1, 600)
         self.planet1 = pg.image.load("recursos/imagenes/planet-450x461.png")
-        self.nave = nave( 10, 275, 0)
+        self.nave = nave(10, 275, 0)
         self.explote = Explote()
 
     # bucle principal   
@@ -252,6 +279,7 @@ class Game:
 
         while not game_over:
             # Gestión de eventos
+
             events = pg.event.get()
             self.clock.tick(FPS)
             for event in events:
@@ -265,18 +293,16 @@ class Game:
                 # Zona de Actualización de elementos del juego
 
                 self.nave.actualizar()
-
                 self.nivel.actualizarAsters()
                 self.nivel.actualizarBigAsters()
 
+                # Zona de pintado de elementos
 
-
-                # Zona de pintado de elementos 
-                
                 # self.pantalla.fill((11, 44, 94))       
                 self.pantalla.blit(self.bg, (0, 0))
 
                 # colision
+
                 allAsters = []
                 for aster in self.nivel.aster:
                     allAsters.append(aster.rect)
@@ -286,40 +312,47 @@ class Game:
                     self.pantalla.blit(bigAster.image, (bigAster.rect.x, bigAster.rect.y))
 
                 if self.nave.rect.collidelistall(allAsters):
-                    self.explote.explote_sound()    
+                    self.explote.explote_sound()
                     self.stop_level = True
                     self.crash_nave = True
                     allAsters = []
                 self.pantalla.blit(self.nave.image, (self.nave.x, self.nave.y))
                 # pg.draw.rect(self.pantalla,(255, 0, 0), self.goalRect) # control de puntos
                 if self.goalRect.collidelistall(allAsters):
-                   self.puntos += 1
+                    self.puntos += 1
 
                 if self.puntos > self.nivel.meta_nivel:
-                    self.finalizando = True
+                    self.nivel.finalizando = True
                 if self.puntos > self.nivel.meta_nivel and not self.nivel.tieneAsteroides():
                     self.stop_level = True
                     self.finish_level = True
 
-        
             if self.stop_level:
-                if self.crash_nave:                  
-                    SURF.blit( create_font("Perdiste", 32, (255, 255, 255)), ((GAME_DIMENSIONS[0] / 2) ,(GAME_DIMENSIONS)[1] / 2))
+                if self.crash_nave:
+                    SURF.blit(create_font("Perdiste", 32, (255, 255, 255)),
+                              ((GAME_DIMENSIONS[0] / 2), (GAME_DIMENSIONS)[1] / 2))
                     contador += 1
                     if contador < len(self.explote.imagenes) * self.explote.retardo_anim:
                         self.pantalla.blit(self.bg, (0, 0))
-                        self.pantalla.blit((self.explote.image), (self.nave.x, self.nave.y - self.nave.getHeight()/ 2))
+                        self.pantalla.blit((self.explote.image), (self.nave.x, self.nave.y - self.nave.getHeight() / 2))
                         self.explote.update()
                     if contador > 500:
                         self.vidas -= 1
                         contador = 0
-                        self.nivel.update_nivel()
+                        self.nivel.restart()
                         self.stop_level = False
                         self.crash_nave = False
+                        self.nivel.finalizando = False
                 elif self.finish_level:
                     self.pantalla.blit(self.bg, (0, 0))
-                    self.pantalla.blit(self.planet1, ((GAME_DIMENSIONS[0] - pg.Surface.get_width(self.planet1) / 2 ), (0 + (GAME_DIMENSIONS[1] - pg.Surface.get_height(self.planet1)) / 2 ) ))
-                    # • Aparecerá un planeta por la parte derecha de la pantalla.
+                    self.pantalla.blit(self.planet1, ((GAME_DIMENSIONS[0] - pg.Surface.get_width(self.planet1) / 2),
+                                                      (0 + (GAME_DIMENSIONS[1] - pg.Surface.get_height(self.planet1)) / 2)))
+                    self.nave.naveAterrizando()
+                    self.pantalla.blit(self.nave.image, (self.nave.x, self.nave.y))
+
+
+
+                    # • Aparecerá un planeta por la parte derecha de la pantalla cuando termine el nivel.
                     # • Dejarán de aparecer obstáculos pero los que estén en pantalla deberán continuar su movimiento hasta
                     # el borde derecho o chocar con la nave.
                     # • La nave girará 180 grados y aterrizará sobre el planeta de forma automática
@@ -327,13 +360,10 @@ class Game:
                     # • Si fuera el último nivel el cartel sería de felicitación e indicaría acción para reiniciar el juego.
                     # • Si el jugador no iniciara la partida transcurrido un tiempo, se volverá a la portada.
 
-
-            
-            SURF.blit( create_font("Nivel:" + str(self.nivel_cont), 32, (255, 255, 255)), ((GAME_DIMENSIONS[0] / 6) * 0, 0))
-            SURF.blit( create_font("Vidas:" + str(self.vidas), 32, (255, 255, 255)), ((GAME_DIMENSIONS[0] / 6) * 2, 0))
-            SURF.blit( create_font("Puntos:" + str(self.puntos), 32, (255, 255, 255)), ((GAME_DIMENSIONS[0] / 6) * 4, 0))
-
+            SURF.blit(create_font("Nivel:" + str(self.nivel_cont), 32, (255, 255, 255)),
+                      ((GAME_DIMENSIONS[0] / 6) * 0, 0))
+            SURF.blit(create_font("Vidas:" + str(self.vidas), 32, (255, 255, 255)), ((GAME_DIMENSIONS[0] / 6) * 2, 0))
+            SURF.blit(create_font("Puntos:" + str(self.puntos), 32, (255, 255, 255)), ((GAME_DIMENSIONS[0] / 6) * 4, 0))
 
             # Zona de refrescar pantalla
             pg.display.flip()
-
